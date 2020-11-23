7e320f06075e9a5a734b39fdd523307fe50f1285
daniela104 <dani_ela090@hotmail.com>
2020-11-23 15:49:20 -0300

    Adicionando estrutura com mais detalhes

--- src/ProgramaNotas.java
+++ src/ProgramaNotas.java
@@ -1,6 +1,8 @@
 public class ProgramaNotas {
 
 	public static void main(String[] parametros) {
+		//int nota = Integer.parseInt(parametros[0]);
+		//if(nota>=7)....
 		if(true){
 			//passou
 		} else {
