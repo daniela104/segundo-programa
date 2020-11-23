1c37db6fc1e66e6984748b0639495e1146a6c761
daniela104 <dani_ela090@hotmail.com>
2020-11-23 15:04:16 -0300

    Subindo estrutura condicial if

---
+++ src/Programa.java
@@ -0,0 +1,14 @@
+public class Programa {
+	public static void main(String[] parametros) {
+		//parametros[0]
+		if(true){ //condicao equals manha
+			//bom dia
+		}
+		if(true){//condicao equals tarde
+			//boa tarde
+		}
+		if(true){//condicao equals noite
+			//boa noite
+		}
+	}
+}
