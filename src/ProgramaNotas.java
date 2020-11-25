293ad161df29e54dc573f0ec7d6d565f84f00d47
daniela104 <dani_ela090@hotmail.com>
2020-11-25 01:20:43 -0300

    primeira lição if else

--- src/ProgramaNotas.java
+++ src/ProgramaNotas.java
@@ -3,11 +3,20 @@ public class ProgramaNotas {
 	public static void main(String[] parametros) {
 		//int nota = Integer.parseInt(parametros[0]);
 		//if(nota>=7)....
-		if(true){
-			//passou
-		} else {
-			//nao passou
+
+
+
+		int nota = Integer.parseInt(parametros[0]);
+
+		if(nota>=7){ 
+		System.out.println("Aluno aprovado"); 
+	
+	
+		} else {	 
+		System.out.println("Aluno reprovado"); 
+	
+
+
 		}
 	}
-
 }
